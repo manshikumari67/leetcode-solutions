9e689970cf22034f8253f4b312d6b10e9226d312
manshikumari67 <manshikumari345@gmail.com>
2025-08-03 20:35:39 +0530

    Leetcode Question 83

---
+++ LinkedList/removeDuplicate_83.java
@@ -0,0 +1,30 @@
+package LinkedList;
+
+// 83.Remove Duplicate from Sorted Linked List 
+
+// Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+
+public class removeDuplicate_83 {
+
+
+     public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+
+     public ListNode deleteDuplicates(ListNode head) {
+        ListNode temp=head;
+
+        while(temp!=null && temp.next!=null){
+            if(temp.val==temp.next.val){
+                temp.next=temp.next.next;
+            }else{
+                temp=temp.next;
+            }
+        }
+
+        return head;
+    
+}
