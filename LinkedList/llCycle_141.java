0fe59db023f61a88024fa5434428def0d1db757a
manshikumari67 <manshikumari345@gmail.com>
2025-08-06 23:42:05 +0530

    LeetCode Question 141

---
+++ LinkedList/llCycle_141.java
@@ -0,0 +1,45 @@
+package LinkedList;
+
+// 141. Linked List Cycle
+// Given head, the head of a linked list, determine if the linked list has a cycle in it.
+// There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+// Return true if there is a cycle in the linked list. Otherwise, return false.
+
+public class llCycle_141 {
+
+     public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
+
+     public boolean hasCycle(ListNode head) {
+        
+       if (head==null) return false;
+
+        if(head.next==null){
+            return false;
+        }
+        
+      ListNode slow=head;
+      ListNode fast=head;
+
+      while(fast!=null){
+        if(slow==null) return false;
+        slow=slow.next;
+        if(fast.next==null) return false;
+        fast=fast.next.next;
+
+        if(fast==slow){
+            return true;
+        }
+      }
+
+      return false;
+
+
+    }
+}
