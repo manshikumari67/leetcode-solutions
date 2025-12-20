5cbd35c77674b6ae7ded35c8bb8769a5dad4b964
manshikumari67 <manshikumari345@gmail.com>
2025-12-20 23:00:14 +0530

    leetcode question 145

---
+++ BinaryTree/BinaryTreePostorderTraversal_145.java
@@ -0,0 +1,39 @@
+package BinaryTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.tree.TreeNode;
+
+// 145. Binary Tree Postorder Traversal
+
+// Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int val){
+        this.val=val;
+    }
+}
+
+
+public class BinaryTreePostorderTraversal_145 {
+    private static void postOrder(TreeNode node,List<Integer> ans){
+        if(node==null) return ;
+
+        postOrder(node.left,ans);
+        postOrder(node.right,ans);
+        ans.add(node.val);
+    }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+         List<Integer> ans=new ArrayList<>();
+
+         postOrder(root,ans);
+
+         return ans;
+    }
+}
