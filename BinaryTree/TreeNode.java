3e32fb01b47c3fbfe143ec04b719915abc478fc1
manshikumari67 <manshikumari345@gmail.com>
2025-12-21 23:03:16 +0530

    leetcode question 94

---
+++ BinaryTree/TreeNode.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+
+public class TreeNode {
+     int val;
+      TreeNode left;
+    TreeNode right;
+     TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+class BinaryTreeInorderTraversal{
+   private static void inOrder(TreeNode node, List<Integer> ans) {
+        if (node == null) {
+            return;
+        }
+
+        inOrder(node.left, ans);
+        ans.add(node.val);
+        inOrder(node.right, ans);
+    }
+    public List<Integer> inorderTraversal(TreeNode root) {
+        
+        List<Integer> ans = new ArrayList<>();
+
+        inOrder(root, ans);
+
+        return ans;      
+    }
+}
