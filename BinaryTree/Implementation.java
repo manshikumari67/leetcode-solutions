a5da03e5f722489b2e2fc9bc2cbbb12b22a058d9
manshikumari67 <manshikumari345@gmail.com>
2025-12-24 22:59:31 +0530

    leetcode question 101 and 112

--- BinaryTree/Implementation.java
+++ BinaryTree/Implementation.java
@@ -1,4 +1,5 @@
-
+import java.util.LinkedList;
+import java.util.Queue;
 
 class Node{
     int val;
@@ -6,6 +7,15 @@ class Node{
     Node right;
     Node(int val){
         this.val=val;
+    }
+}
+
+class Pair{
+    int level;
+    Node node;
+    Pair(Node node,int level){
+        this.node=node;
+        this.level=level;
     }
 }
 
@@ -32,10 +42,52 @@ public class Implementation {
         System.out.println(size(a));
         System.out.println(sum(a)); 
         System.out.println(level(a));
-        
+        levelOrder(a);
+        levelOrderLevelWise(a);
+        kThLevel(a, 0, 0);
     }
 
-     private static int level(Node root){
+    private static void levelOrder(Node root){
+       Queue<Node> q=new LinkedList<>();
+       q.add(root);
+
+       while(q.size()>0){
+        Node front=q.remove();
+        System.out.print(front.val+" ");
+        if(front.left!=null) q.add(front.left);
+        if(front.right!=null) q.add(front.right);
+       }
+       System.out.println();
+    }
+
+    private static void levelOrderLevelWise(Node root){
+       Queue<Pair> q=new LinkedList<>();
+       int currLevel=0;
+       q.add(new Pair(root,0));
+
+       while(q.size()>0){
+        Pair front=q.remove();
+        Node node=front.node;
+        int level=front.level;
+        if(front.level!=currLevel){
+            currLevel++;
+            System.out.println();
+        }
+        System.out.print(node.val+" "); 
+        if(node.left!=null) q.add(new Pair(node.left,level+1 ));
+        if(node.right!=null) q.add(new Pair(node.right,level+1));
+       }
+       System.out.println();
+    }
+
+    private static void kThLevel(Node root, int level, int k){
+        if(root==null) return;
+        if(level==k) System.out.print(root.val+" ");
+        kThLevel(root.left, level+1, k);
+        kThLevel(root.right, level+1, k);
+    }
+
+    private static int level(Node root){
         if(root==null) return 0;
         return 1+Math.max(level(root.left), level(root.right));
     }
