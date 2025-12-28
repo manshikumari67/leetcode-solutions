1218011dad0fb9d9719af84ffca19d1afaff8b53
manshikumari67 <manshikumari345@gmail.com>
2025-12-28 22:23:06 +0530

    leetcode question 257

--- BinaryTree/BinaryTreePaths_257.java
+++ BinaryTree/BinaryTreePaths_257.java
@@ -5,6 +5,9 @@
 // Given the root of a binary tree, return all root-to-leaf paths in any order.
 // A leaf is a node with no children.
 
+import java.util.ArrayList;
+import java.util.List;
+
 class TreeNode {
       int val;
       TreeNode left;
@@ -19,5 +22,27 @@ class TreeNode {
 }
 
 public class BinaryTreePaths_257 {
-    
+     public List<String> binaryTreePaths(TreeNode root) {
+        List<String> ans = new ArrayList<>();
+        if (root == null) return ans;
+        dfs(root, "", ans);
+        return ans;
+    }
+
+    private void dfs(TreeNode node, String path, List<String> ans) {
+        if (node == null) return;
+
+        // current node ko path me add karo
+        path = path + node.val;
+
+        // agar leaf node hai
+        if (node.left == null && node.right == null) {
+            ans.add(path);
+            return;
+        }
+
+        // children ke liye arrow add karke call
+        dfs(node.left, path + "->", ans);
+        dfs(node.right, path + "->", ans);
+    }
 }
