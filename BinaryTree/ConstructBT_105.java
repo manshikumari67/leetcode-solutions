9cf000bea72dda4445a12fcedc63334670194dcc
manshikumari67 <manshikumari345@gmail.com>
2025-12-29 23:34:51 +0530

    leetcode question 105

---
+++ BinaryTree/ConstructBT_105.java
@@ -0,0 +1,42 @@
+
+
+// 105. Construct Binary Tree from PreOrder and Inorder
+
+// Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+        }
+}
+
+
+public class ConstructBT_105 {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+       int n =inorder.length;
+       return build(0,n-1,0,n-1,inorder,preorder);
+    }
+    public static TreeNode build(int preLo,int preHi,int inLo,int inHi,int[] inorder,int[] preorder){
+        if(preLo>preHi ||inLo > inHi) return null;
+        int val=preorder[preLo];
+        TreeNode root=new TreeNode(val);
+        int r=-1;
+        for(int i=inLo;i<=inHi;i++){
+            if(inorder[i]==val){
+                r=i;
+                break;
+            }
+        }
+        int cnt=r-inLo;
+        root.left=build(preLo+1, preLo+cnt , inLo,r-1, inorder,preorder);
+        root.right=build(preLo+cnt+1, preHi ,r+1,inHi, inorder,preorder);
+        return root;
+    }
+}
