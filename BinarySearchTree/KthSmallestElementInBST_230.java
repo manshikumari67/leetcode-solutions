a0cddc50dc9734846a6122b7f0a5cc709529e5de
manshikumari67 <manshikumari345@gmail.com>
2026-01-09 19:14:47 +0530

    leetcode question 230

---
+++ BinarySearchTree/KthSmallestElementInBST_230.java
@@ -0,0 +1,22 @@
+package BinarySearchTree;
+
+import java.util.ArrayList;
+
+// 230.Kth Smallest Element in a BST
+
+// Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+
+public class KthSmallestElementInBST_230 {
+     public void inorder(TreeNode root, ArrayList<Integer> arr){
+        if(root==null) return;
+        inorder(root.left,arr);
+        arr.add(root.val);
+        inorder(root.right,arr);
+    }
+
+    public int kthSmallest(TreeNode root, int k) {
+        ArrayList<Integer> arr=new ArrayList<>();
+        inorder(root,arr);
+        return arr.get(k-1);
+    }
+}
