b7df139392b7cbb6ad29fed6e4c5a497ffbc2230
manshikumari67 <manshikumari345@gmail.com>
2026-01-09 19:09:30 +0530

    leetcode question 938

---
+++ BinarySearchTree/RangeSumOfBST_938.java
@@ -0,0 +1,16 @@
+package BinarySearchTree;
+
+// 938. Range Sum of BST
+
+// Given the root node of a binary search tree and two integers low and high, return the sum of values of all nodes with a value in the inclusive range [low, high].
+
+public class RangeSumOfBST_938 {
+     public int rangeSumBST(TreeNode root, int low, int high) {
+        if(root==null) return 0;
+        if(root.val<low) return rangeSumBST(root.right,low,high);
+        else if(root.val > high) return rangeSumBST(root.left,low,high);
+        else{
+            return root.val+rangeSumBST(root.left,low,high)+rangeSumBST(root.right,low,high);
+        }
+    }
+}
