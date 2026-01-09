d6132b38cd3cc38a4e29403a85a3d0e469a405da
manshikumari67 <manshikumari345@gmail.com>
2026-01-09 19:11:43 +0530

    leetcode question 701

---
+++ BinarySearchTree/InsetIntoBST_701.java
@@ -0,0 +1,26 @@
+package BinarySearchTree;
+
+// 701. Insert Into A Binary Tree
+
+// You are given the root node of a binary search tree (BST) and a value to insert into the tree. Return the root node of the BST after the insertion. It is guaranteed that the new value does not exist in the original BST.
+// Notice that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion. You can return any of them
+
+public class InsetIntoBST_701 {
+     void attach(TreeNode root, int key){
+      if(root.val==key) return;
+      if(root.val<key){
+        if(root.right==null) root.right=new TreeNode(key);
+        else attach(root.right,key);
+      }
+      else{
+        if(root.left==null) root.left=new TreeNode(key);
+        else attach(root.left,key);
+      }
+    }
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) return new TreeNode(val);
+        attach(root,val);
+        return root;
+    }
+}
