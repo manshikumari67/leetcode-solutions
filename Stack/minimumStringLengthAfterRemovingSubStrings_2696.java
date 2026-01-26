6e5ec086158b88fc760bc8ce846948ce67a7cd52
manshikumari67 <manshikumari345@gmail.com>
2026-01-26 22:10:11 +0530

    Leetcode Question 387

---
+++ Stack/minimumStringLengthAfterRemovingSubStrings_2696.java
@@ -0,0 +1,34 @@
+
+// 2696. Minimum String Length After Removing SubStrings
+
+// You are given a string s consisting only of uppercase English letters.
+// You can apply some operations to this string where, in one operation, you can remove any occurrence of one of the substrings "AB" or "CD" from s.
+// Return the minimum possible length of the resulting string that you can obtain.
+
+
+class minimumStringLengthAfterRemovingSubStrings_2696 {
+    public int minLength(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char cur_char = s.charAt(i);
+
+            if (stack.isEmpty()) {
+                stack.push(cur_char);
+                continue;
+            }
+      
+            if (cur_char == 'B' && stack.peek() == 'A') {
+                stack.pop();
+            }
+            else if (cur_char == 'D' && stack.peek() == 'C') {
+                stack.pop();
+            }
+            else {
+                stack.push(cur_char);
+            }
+        }
+
+        return stack.size();
+    }
+}
