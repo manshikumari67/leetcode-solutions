389773963e62d1c61c9c54d8475d2a0113710581
manshikumari67 <manshikumari345@gmail.com>
2026-02-04 13:06:26 +0530

    Leetcode Question 1382

---
+++ BinarySearchTree/BalanceABinarySearchTree_1382.java
@@ -0,0 +1,28 @@
+// 1328. Balance A Binary Search Tree
+
+// Given the root of a binary search tree, return a balanced binary search tree with the same node values. If there is more than one answer, return any of them.
+
+
+A binary search tree is balanced if the depth of the two subtrees of every node never differs by more than 1
+
+class BalanceABinarySearchTree_1382 {
+    private List<TreeNode> sortedArr = new ArrayList<>();
+    public TreeNode balanceBST(TreeNode root) {
+        inorderTraverse(root);
+        return sortedArrayToBST(0, sortedArr.size() - 1);
+    }
+    private void inorderTraverse(TreeNode root) {
+        if (root == null) return;
+        inorderTraverse(root.left);
+        sortedArr.add(root);
+        inorderTraverse(root.right);
+    }
+    private TreeNode sortedArrayToBST(int start, int end) {
+        if (start > end) return null;
+        int mid = (start + end) / 2;
+        TreeNode root = sortedArr.get(mid);
+        root.left = sortedArrayToBST(start, mid - 1);
+        root.right = sortedArrayToBST(mid + 1, end);
+        return root;
+    }
+}
