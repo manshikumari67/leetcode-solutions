a8934d9d662b5b54156043b7382b108b5be3aff5
manshikumari67 <manshikumari345@gmail.com>
2026-02-11 15:27:32 +0530

    Find XOR of Numbers From L to R

---
+++ BitManipulation/FindXORofNumbersFromLtoR.java
@@ -0,0 +1,11 @@
+public class FindXORofNumbersFromLtoR {
+    public static int xor(int n){
+        if(n%4==1) return 1;
+        else if(n%4==2) return n+1;
+        else if(n%4==3) return 0;
+        else return n;
+    }
+    public static int findXOR(int l, int r){
+        return xor(r) ^ xor(l-1);
+    }
+}
