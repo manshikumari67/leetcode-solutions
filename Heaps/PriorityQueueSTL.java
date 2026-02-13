47e27ac0d502efd7d5211f52830934dcaf231e1e
manshikumari67 <manshikumari345@gmail.com>
2026-02-13 23:24:15 +0530

    PriorityQueueSTL

---
+++ Heaps/PriorityQueueSTL.java
@@ -0,0 +1,25 @@
+package Heaps;
+
+import java.util.PriorityQueue;
+
+public class PriorityQueueSTL {
+     public static void main(String[] args) {
+
+        // Minheap
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        pq.add(10);
+        pq.add(20);
+        pq.add(-30);
+
+        System.out.println(pq.peek());
+
+        pq.add(35);
+
+        System.out.println(pq.peek());
+        System.out.println(pq.size());
+        System.out.println(pq.remove());
+        System.out.println(pq.peek());
+        System.out.println(pq);
+    }
+}
