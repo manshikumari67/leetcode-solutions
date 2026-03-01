b1a5a1f5587532f4e80f88e9334b9866e4062ac7
manshikumari67 <manshikumari345@gmail.com>
2026-03-01 22:06:41 +0530

    leetcode question 118

---
+++ DP/PascalsTriangle_118.java
@@ -0,0 +1,26 @@
+package DP;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+// 118. Pascal's Triangle
+
+// Given an integer numRows, return the first numRows of Pascal's triangle.
+
+public class PascalsTriangle_118 {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> pascal = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>(Collections.nCopies(i + 1, 1));
+            
+            for (int j = 1; j < i; j++) {
+                int val = pascal.get(i - 1).get(j - 1) + pascal.get(i - 1).get(j);
+                row.set(j, val);
+            }
+            pascal.add(row);
+        }
+        return pascal;
+    }
+}
