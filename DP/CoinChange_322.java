b771d1a84e9f0132364e7ee142e72bd835e9e106
manshikumari67 <manshikumari345@gmail.com>
2026-03-20 23:13:47 +0530

    leetcode Question 322

---
+++ DP/CoinChange_322.java
@@ -0,0 +1,33 @@
+package DP;
+
+// 322. Coin Change
+
+// You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+// Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+// You may assume that you have an infinite number of each kind of coin. 
+
+public class CoinChange_322 {
+     public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        int[][] dp = new int[n + 1][amount + 1];
+
+        // Base Case: 0 coins needed for amount 0
+        for (int i = 0; i <= n; i++) dp[i][0] = 0;
+        // Base Case: Infinity coins for amount > 0 with 0 coin types
+        for (int j = 1; j <= amount; j++) dp[0][j] = Integer.MAX_VALUE - 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                if (coins[i - 1] <= j) {
+                    // Min of (Include current coin + 1, Exclude current coin)
+                    dp[i][j] = Math.min(1 + dp[i][j - coins[i - 1]], dp[i - 1][j]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        
+        int result = dp[n][amount];
+        return result >= Integer.MAX_VALUE - 1 ? -1 : result;
+    }
+}
