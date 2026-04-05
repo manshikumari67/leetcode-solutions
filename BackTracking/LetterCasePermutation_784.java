a060e15447ffa20612afd90c8db90d3626bbf4a0
manshikumari67 <manshikumari345@gmail.com>
2026-04-05 21:40:26 +0530

    Leetcode Question 784

---
+++ BackTracking/LetterCasePermutation_784.java
@@ -0,0 +1,48 @@
+package BackTracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 784. Letter Case Permutation
+
+// Given a string s, you can transform every letter individually to be lowercase or uppercase to create another string.
+// Return a list of all possible strings we could create. Return the output in any order.
+
+public class LetterCasePermutation_784 {
+     void recurse(char[] str, int pos, List<String> result) {
+        //If we have reached a leaf in the recursion tree, save the result.
+        if (pos == str.length) {
+            result.add(new String(str));
+            return;
+        }
+        
+        //If char is not a letter, no processing required.
+        if (Character.isLetter(str[pos])) {
+            //If uppercase char, we make it lower case, and recurse.
+            if (Character.isUpperCase(str[pos])) {
+                str[pos] = Character.toLowerCase(str[pos]);
+                
+                //Start a new branch in the recursion tree, exploring options that are possible only if we had changed the case.
+                recurse(str, pos + 1, result);
+                
+                //Backtracking. We undo the change so that we can start a new branch in the recursion tree.
+                str[pos] = Character.toUpperCase(str[pos]);
+            }
+            //If lowercase, then we make it upper case, and recurse.
+            else {
+                str[pos] = Character.toUpperCase(str[pos]);
+                recurse(str, pos + 1, result);
+                //Backtracking as explained above.
+                str[pos] = Character.toLowerCase(str[pos]);
+            }
+        }
+        //This branch explores options that are possible only if the previously performed change (if any) hadn't happened.
+        recurse(str, pos + 1,  result);
+    }
+    
+    public List<String> letterCasePermutation(String S) {
+        List<String> result = new ArrayList<>();
+        recurse(S.toCharArray(), 0, result);
+        return result;
+    }
+}
