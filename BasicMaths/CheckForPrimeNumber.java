5235886011895f695221553bea7077c2d347009d
manshikumari67 <manshikumari345@gmail.com>
2026-04-23 19:21:55 +0530

    Check For Prime Numbers

---
+++ BasicMaths/CheckForPrimeNumber.java
@@ -0,0 +1,20 @@
+package BasicMaths;
+
+// Check for Prime Number
+
+// You are given an integer n. You need to check if the number is prime or not. Return true if it is a prime number, otherwise return false.
+// A prime number is a number which has no divisors except 1 and itself.
+
+public class CheckForPrimeNumber {
+    public boolean isPrime(int n) {
+          //your code goes here
+          if (n <= 1) return false;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
