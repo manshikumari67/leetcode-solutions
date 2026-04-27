e694f7d285f3971f3ece22de5f9940f7c1a97381
manshikumari67 <manshikumari345@gmail.com>
2026-04-27 22:54:10 +0530

    LeetCode Question 134

---
+++ Array/GasStation_134.java
@@ -0,0 +1,30 @@
+package Array;
+
+// 134. Gas Station
+
+// There are n gas stations along a circular route, where the amount of gas at the ith station is gas[i].
+// You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from the ith station to its next (i + 1)th station. You begin the journey with an empty tank at one of the gas stations.
+// Given two integer arrays gas and cost, return the starting gas station's index if you can travel around the circuit once in the clockwise direction, otherwise return -1. If there exists a solution, it is guaranteed to be unique.
+
+
+public class GasStation_134 {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+    int total = 0;
+    int tank = 0;
+    int start = 0;
+
+    for (int i = 0; i < gas.length; i++) {
+        int diff = gas[i] - cost[i];
+        
+        total += diff;
+        tank += diff;
+
+        if (tank < 0) {
+            start = i + 1;
+            tank = 0;
+        }
+    }
+
+    return total >= 0 ? start : -1;
+}
+}
