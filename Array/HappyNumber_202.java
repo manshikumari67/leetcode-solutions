cb67e1fdea31b84b2be54b782c8a449c84b400d5
manshikumari67 <manshikumari345@gmail.com>
2026-06-10 23:43:22 +0530

    leetcode Question 202

---
+++ Array/HappyNumber_202.java
@@ -0,0 +1,26 @@
+package Array;
+
+// 202. Happy Number
+
+// Write an algorithm to determine if a number n is happy.
+// A happy number is a number defined by the following process:
+// Starting with any positive integer, replace the number by the sum of the squares of its digits.
+// Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+// Those numbers for which this process ends in 1 are happy.
+// Return true if n is a happy number, and false if not.
+
+public class HappyNumber_202 {
+    public boolean isHappy(int n) {
+        if(n==1 || n==7) return true;
+        else if(n<10) return false;
+        else{
+            int sum=0;
+            while(n>0){
+                int temp=n%10;
+                sum+= temp*temp;
+                n=n/10;
+            }
+            return isHappy(sum);
+        }
+    }
+}
